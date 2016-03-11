9518408c2ef6ae163a2461c19d048718ca780de8
Andrey Fedorov <fedorov@bwh.harvard.edu>
2016-03-11 15:46:03 -0500

    BUG: functionality fixes
    * casting is redundant - can read as short (and I could not figure out why the
    image returned by cast->GetOutput() was not a pointer!)
    * ImageType::Pointer.GetPointer() is (probably) the pointer to the class, while
    what we need is the pointer to the image buffer
    
    With these fixes, output log shows that there is some computation for the test data,
    although the resulting matrix is still identity
    
    ZFrameCalibration standard error:
    
    === Image Size (x,y,z): 256, 256, 20===
    === Current Slice Index: 6===
    === Image position ===
    x = -10.3865
    y = 15.232
    z = -112.19
    ZTrackerTransform - Searching fiducials...
    
    ZTrackerTransform - Checking the fiducial geometries...
    
    Rotation Angle [degrees]: 179.241
    
    Rotation Axis: [-0.000121127,0.00691737,-0.999976]
    
    Displacement [mm]: [0.27705,-0.370695,5.44905]
    
    position = (-10.6635, 15.6027, -106.741)
    quaternion = (-0.00691721, -0.000121125, 0.00662897, 0.999954)
    === Current Slice Index: 7===
    === Image position ===
    x = -10.3865
    y = 15.232
    z = -109.79
    ZTrackerTransform - Searching fiducials...
    
    ZTrackerTransform - Checking the fiducial geometries...
    
    Rotation Angle [degrees]: 179.234
    
    Rotation Axis: [-0.00416491,0.010709,-0.999934]
    
    Displacement [mm]: [0.437155,-0.565458,2.99691]
    
    position = (-10.8236, 15.7974, -106.793)
    quaternion = (-0.0107088, -0.00416482, 0.00668192, 0.999912)
    === Current Slice Index: 8===
    === Image position ===
    x = -10.3865
    y = 15.232
    z = -107.39
    ZTrackerTransform - Searching fiducials...
    
    ZTrackerTransform - Checking the fiducial geometries...
    
    Rotation Angle [degrees]: 179.339
    
    Rotation Axis: [-0.00139453,0.0102494,-0.999947]
    
    Displacement [mm]: [0.498473,-0.338506,0.531095]
    
    position = (-10.8849, 15.5705, -106.859)
    quaternion = (-0.0102492, -0.0013945, 0.00576765, 0.99993)
    === Current Slice Index: 9===
    === Image position ===
    x = -10.3865
    y = 15.232
    z = -104.99
    ZTrackerTransform - Searching fiducials...
    
    ZTrackerTransform - Checking the fiducial geometries...
    
    Rotation Angle [degrees]: 180.703
    
    Rotation Axis: [0.00308776,-0.00906482,0.999954]
    
    Displacement [mm]: [0.528813,-0.561562,-1.95842]
    
    position = (-10.9153, 15.7935, -106.949)
    quaternion = (0.00906465, 0.0030877, -0.00613094, -0.999935)
    === Current Slice Index: 10===
    === Image position ===
    x = -10.3865
    y = 15.232
    z = -102.59
    ZTrackerTransform - Searching fiducials...
    
    ZTrackerTransform - Checking the fiducial geometries...
    
    Rotation Angle [degrees]: 180.673
    
    Rotation Axis: [0.0029843,-0.00624284,0.999976]
    
    Displacement [mm]: [0.511596,-0.600536,-4.31572]
    
    position = (-10.8981, 15.8325, -106.906)
    quaternion = (0.00624273, 0.00298425, -0.00587217, -0.999959)
    T[0, 0] = (7.77423e-05, 2.12699e-05, -5.37513e-05, -0.00863595)
    T[1, 0] = (2.12699e-05, 7.54893e-06, -1.46259e-05, -0.00235032)
    T[2, 0] = (-5.37513e-05, -1.46259e-05, 3.87856e-05, 0.00621594)
    T[3, 0] = (-0.00863595, -0.00235032, 0.00621594, 0.999876)
    V[0, 0] = (0.0224773, 0.498101, -0.866785, -0.00863654)
    V[1, 0] = (-0.00381936, -0.866995, -0.498297, -0.00235049)
    V[2, 0] = (0.999722, -0.0144972, 0.0175319, 0.00621635)
    V[3, 0] = (-0.00602983, 0.00235427, -0.00876677, 0.999941)
    average position = (-10.8371, 15.7193, -106.85)
    average orientation = (-0.00863654, -0.00235049, 0.00621635, 0.999941)
    Result matrix:

--- ZFrameCalibration.cxx
+++ ZFrameCalibration.cxx
@@ -43,7 +43,7 @@ template<class T> int DoIt( int argc, char * argv[], T )
 
     const unsigned int Dimension = 3;
 
-    typedef float PixelType;
+    typedef short PixelType;
 
     typedef itk::Image<PixelType, Dimension> ImageType;
     typedef itk::ImageFileReader<ImageType> ReaderType;
@@ -53,12 +53,6 @@ template<class T> int DoIt( int argc, char * argv[], T )
 
     reader->SetFileName(inputVolume.c_str());
     reader->Update();
-
-    typedef itk::Image<short, Dimension>  ShortImageType;
-    typedef itk::CastImageFilter<ImageType, ShortImageType> CastFilterType;
-    CastFilterType::Pointer castFilter = CastFilterType::New();
-    castFilter->SetInput(reader->GetOutput());
-    castFilter->Update();
 
     ImageType::Pointer image = reader->GetOutput();
 
@@ -143,9 +137,6 @@ template<class T> int DoIt( int argc, char * argv[], T )
     range[0] = startSlice;
     range[1] = endSlice;
 
-    ShortImageType::Pointer shortImage = castFilter->GetOutput();
-    short* inputImage = (short*)shortImage.GetPointer();
-
     float Zposition[3];
     float Zorientation[4];
 
@@ -158,7 +149,7 @@ template<class T> int DoIt( int argc, char * argv[], T )
     dim[1] = dimensions[1];
     dim[2] = dimensions[2];
 
-    calibration->SetInputImage(inputImage, dim, imageTransform);
+    calibration->SetInputImage(image->GetBufferPointer(), dim, imageTransform);
     calibration->SetOrientationBase(ZquaternionBase);
     int r = calibration->Register(range, Zposition, Zorientation);
 
@@ -281,3 +272,39 @@ int main( int argc, char * argv[] )
     }
     return EXIT_SUCCESS;
 }
+
+#if 0
+//----------------------------------------------------------------------------
+bool ConvertItkImageToVtkImageData(typename itk::Image<T, 3>::Pointer inItkImage, vtkImageData* outVtkImageData, int vtkType)
+{
+  if ( outVtkImageData == NULL )
+  {
+    std::cerr << "SlicerRtCommon::ConvertItkImageToVtkImageData: Output VTK image data is NULL!" << std::endl;
+    return false;
+  }
+
+  if ( inItkImage.IsNull() )
+  {
+    vtkErrorWithObjectMacro(outVtkImageData, "ConvertItkImageToVtkImageData: Input ITK image is invalid!");
+    return false;
+  }
+
+  typename itk::Image<T, 3>::RegionType region = inItkImage->GetBufferedRegion();
+  typename itk::Image<T, 3>::SizeType imageSize = region.GetSize();
+  int extent[6]={0, (int) imageSize[0]-1, 0, (int) imageSize[1]-1, 0, (int) imageSize[2]-1};
+  outVtkImageData->SetExtent(extent);
+  outVtkImageData->AllocateScalars(vtkType, 1);
+
+  T* outVtkImageDataPtr = (T*)outVtkImageData->GetScalarPointer();
+  typename itk::ImageRegionIteratorWithIndex< itk::Image<T, 3> > itInItkImage(
+    inItkImage, inItkImage->GetLargestPossibleRegion() );
+  for ( itInItkImage.GoToBegin(); !itInItkImage.IsAtEnd(); ++itInItkImage )
+  {
+    typename itk::Image<T, 3>::IndexType i = itInItkImage.GetIndex();
+    (*outVtkImageDataPtr) = inItkImage->GetPixel(i);
+    outVtkImageDataPtr++;
+  }
+
+  return true;
+}
+#endif
