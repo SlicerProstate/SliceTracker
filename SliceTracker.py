78f3b7eb8e75aff1bc0c5779cc0ffa60ffa7c74c
Christian Herz <christianherz1985@gmail.com>
2016-06-15 19:05:58 -0400

    ENH: Display changes of computed hole in target table (issue #158) - highlighted with coloring - tooltip if change detected

--- SliceTracker.py
+++ SliceTracker.py
@@ -11,13 +11,15 @@ from slicer.ScriptedLoadableModule import *
 import EditorLib
 from Editor import EditorWidget
 
+
 from SlicerProstateUtils.constants import DICOMTAGS, COLOR, STYLE, FileExtension
-from SlicerProstateUtils.helpers import SmartDICOMReceiver, SliceAnnotation, CustomTargetTableModel, TargetCreationWidget
+from SlicerProstateUtils.helpers import SmartDICOMReceiver, SliceAnnotation, TargetCreationWidget
 from SlicerProstateUtils.helpers import RatingWindow, IncomingDataMessageBox, IncomingDataWindow
 from SlicerProstateUtils.helpers import WatchBoxAttribute, BasicInformationWatchBox, DICOMBasedInformationWatchBox
 from SlicerProstateUtils.mixins import ModuleWidgetMixin, ModuleLogicMixin, ParameterNodeObservationMixin
 from SlicerProstateUtils.events import SlicerProstateEvents
 
+# from SliceTrackerUtils.helpers import CustomTargetTableModel
 from SliceTrackerUtils.events import SliceTrackerEvents
 from SliceTrackerUtils.constants import SliceTrackerConstants
 from SliceTrackerUtils.exceptions import DICOMValueError
@@ -194,6 +196,12 @@ class SliceTrackerWidget(ModuleWidgetMixin, SliceTrackerConstants, ScriptedLoada
   def currentTargets(self, targets):
     self._currentTargets = targets
     self.targetTableModel.targetList = targets
+    if not targets:
+      self.targetTableModel.coverProstateTargetList = None
+    else:
+      coverProstate = self.registrationResults.getMostRecentApprovedCoverProstateRegistration()
+      if coverProstate:
+        self.targetTableModel.coverProstateTargetList = coverProstate.approvedTargets
     self.targetTable.enabled = targets is not None
 
   @currentCaseDirectory.setter
@@ -3344,3 +3352,146 @@ class SliceTrackerTest(ScriptedLoadableModuleTest):
     your test should break so they know that the feature is needed.
     """
     logging.debug(' ___ performing selfTest ___ ')
+
+
+class CustomTargetTableModel(qt.QAbstractTableModel, ParameterNodeObservationMixin):
+
+  COLUMN_NAME = 'Name'
+  COLUMN_2D_DISTANCE = 'Distance 2D[mm]'
+  COLUMN_3D_DISTANCE = 'Distance 3D[mm]'
+  COLUMN_HOLE = 'Hole'
+  COLUMN_DEPTH = 'Depth[mm]'
+
+  headers = [COLUMN_NAME, COLUMN_2D_DISTANCE, COLUMN_3D_DISTANCE, COLUMN_HOLE, COLUMN_DEPTH]
+
+  @property
+  def targetList(self):
+    return self._targetList
+
+  @targetList.setter
+  def targetList(self, targetList):
+    self.needleStartEndPositions = {}
+    if self._targetList and self.observer:
+      self._targetList.RemoveObserver(self.observer)
+    self._targetList = targetList
+    if self._targetList:
+      self.observer = self._targetList.AddObserver(self._targetList.PointModifiedEvent, self.computeNewDepthAndHole)
+    self.computeNewDepthAndHole()
+    self.reset()
+
+  @property
+  def coverProstateTargetList(self):
+    return self._coverProstateTargetList
+
+  @coverProstateTargetList.setter
+  def coverProstateTargetList(self, targetList):
+    self._coverProstateTargetList = targetList
+
+  @property
+  def cursorPosition(self):
+    return self._cursorPosition
+
+  @cursorPosition.setter
+  def cursorPosition(self, cursorPosition):
+    self._cursorPosition = cursorPosition
+    self.dataChanged(self.index(0, 1), self.index(self.rowCount()-1, 2))
+
+  def __init__(self, logic, targets=None, parent=None, *args):
+    qt.QAbstractTableModel.__init__(self, parent, *args)
+    self.logic = logic
+    self._cursorPosition = None
+    self._targetList = None
+    self.needleStartEndPositions = {}
+    self.targetList = targets
+    self.computeCursorDistances = False
+    self.zFrameDepths = {}
+    self.zFrameHole = {}
+    self.observer = None
+
+  def headerData(self, col, orientation, role):
+    if orientation == qt.Qt.Horizontal and role in [qt.Qt.DisplayRole, qt.Qt.ToolTipRole]:
+        return self.headers[col]
+    return None
+
+  def rowCount(self):
+    try:
+      number_of_targets = self.targetList.GetNumberOfFiducials()
+      return number_of_targets
+    except AttributeError:
+      return 0
+
+  def columnCount(self):
+    return len(self.headers)
+
+  def data(self, index, role):
+    row = index.row()
+    col = index.column()
+
+    if role in [qt.Qt.BackgroundRole, qt.Qt.ToolTipRole] \
+            and self.coverProstateTargetList and not self.coverProstateTargetList is self.targetList:
+      if col == 3:
+        targetPosition = [0.0, 0.0, 0.0]
+        self.targetList.GetNthFiducialPosition(row, targetPosition)
+        coverProstateTargetPosition = [0.0, 0.0, 0.0]
+        self.coverProstateTargetList.GetNthFiducialPosition(row, coverProstateTargetPosition)
+        coverProstateHole = self.computeZFrameHole(row, coverProstateTargetPosition)
+        currentHole = self.computeZFrameHole(row, targetPosition)
+        if currentHole == coverProstateHole:
+          return qt.QColor(qt.Qt.green) if role == qt.Qt.BackgroundRole else ""
+        else:
+          return qt.QColor(qt.Qt.red) if role == qt.Qt.BackgroundRole else "Hole changed: was %s" % coverProstateHole
+
+    if not index.isValid() or role not in [qt.Qt.DisplayRole, qt.Qt.ToolTipRole]:
+      return None
+
+    targetPosition = [0.0, 0.0, 0.0]
+    if col in [1,2,3,4]:
+      self.targetList.GetNthFiducialPosition(row, targetPosition)
+
+    if col == 0:
+      return self.targetList.GetNthFiducialLabel(row)
+    elif (col == 1 or col == 2) and self.cursorPosition and self.computeCursorDistances:
+      if col == 1:
+        distance2D = self.logic.get3DDistance(targetPosition, self.cursorPosition)
+        distance2D = [str(round(distance2D[0], 2)), str(round(distance2D[1], 2)), str(round(distance2D[2], 2))]
+        return 'x=' + distance2D[0] + ' y=' + distance2D[1] + ' z=' + distance2D[2]
+      distance3D = self.logic.get3DEuclideanDistance(targetPosition, self.cursorPosition)
+      return str(round(distance3D, 2))
+
+    elif (col == 3 or col == 4) and self.logic.zFrameRegistrationSuccessful:
+      if col == 3:
+        return self.computeZFrameHoleAndSave(row, targetPosition)
+      else:
+        return self.computeZFrameDepth(row, targetPosition)
+    return ""
+
+  def computeZFrameHoleAndSave(self, index, targetPosition):
+    if index not in self.zFrameHole.keys():
+      (start, end, indexX, indexY, depth, inRange) = self.logic.computeNearestPath(targetPosition)
+      self.needleStartEndPositions[index] = (start, end)
+      self.zFrameHole[index] = '(%s, %s)' % (indexX, indexY)
+    return self.zFrameHole[index]
+
+  def computeZFrameHole(self, index, targetPosition):
+    (start, end, indexX, indexY, depth, inRange) = self.logic.computeNearestPath(targetPosition)
+    return'(%s, %s)' % (indexX, indexY)
+
+  def computeZFrameDepth(self, index, targetPosition):
+    if index not in self.zFrameDepths.keys():
+      (start, end, indexX, indexY, depth, inRange) = self.logic.computeNearestPath(targetPosition)
+      self.zFrameDepths[index] = '%.3f' % depth if inRange else '(%.3f)' % depth
+    return self.zFrameDepths[index]
+
+  def computeNewDepthAndHole(self, observer=None, caller=None):
+    self.zFrameDepths = {}
+    self.zFrameHole = {}
+    if not self.targetList or not self.logic.zFrameRegistrationSuccessful:
+      return
+
+    for index in range(self.targetList.GetNumberOfFiducials()):
+      pos = [0.0, 0.0, 0.0]
+      self.targetList.GetNthFiducialPosition(index, pos)
+      self.computeZFrameHoleAndSave(index, pos)
+
+    self.dataChanged(self.index(0, 3), self.index(self.rowCount()-1, 4))
+    self.invokeEvent(vtk.vtkCommand.ModifiedEvent)
