39dbfbcf45a1d9b1155cf2be60c76c26e15933cc
Christian Herz <christianherz1985@gmail.com>
2016-07-13 13:08:35 -0400

    ENH: Added check for already existing Slicer settings for SliceTracker (issue #162) - if setting doesn't exist, write default value to Slicer settings

--- SliceTracker/SliceTrackerUtils/configuration.py
+++ SliceTracker/SliceTrackerUtils/configuration.py
@@ -15,14 +15,24 @@ class SliceTrackerConfiguration(ModuleWidgetMixin):
     config = ConfigParser.RawConfigParser()
     config.read(self.configFile)
 
-    self.setSetting("ZFrame_Registration_Class_Name", config.get('ZFrame Registration', 'class'))
-    self.setSetting("COVER_PROSTATE", config.get('Series Descriptions', 'COVER_PROSTATE'))
-    self.setSetting("COVER_TEMPLATE", config.get('Series Descriptions', 'COVER_TEMPLATE'))
-    self.setSetting("NEEDLE_IMAGE", config.get('Series Descriptions', 'NEEDLE_IMAGE'))
-    self.setSetting("VIBE_IMAGE", config.get('Series Descriptions', 'VIBE_IMAGE'))
-    self.setSetting("Rating_Enabled", config.getboolean('Rating', 'Enabled'))
-    self.setSetting("Maximum_Rating_Score", config.getint('Rating', 'Maximum_Score'))
+    if not self.getSetting("ZFrame_Registration_Class_Name"):
+      self.setSetting("ZFrame_Registration_Class_Name", config.get('ZFrame Registration', 'class'))
 
-    colorFilename = config.get('Color File', 'Filename')
-    self.setSetting("Color_File_Name", os.path.join(os.path.dirname(inspect.getfile(self.__class__)),
+    if not self.getSetting("COVER_PROSTATE"):
+      self.setSetting("COVER_PROSTATE", config.get('Series Descriptions', 'COVER_PROSTATE'))
+    if not self.getSetting("COVER_TEMPLATE"):
+      self.setSetting("COVER_TEMPLATE", config.get('Series Descriptions', 'COVER_TEMPLATE'))
+    if not self.getSetting("NEEDLE_IMAGE"):
+      self.setSetting("NEEDLE_IMAGE", config.get('Series Descriptions', 'NEEDLE_IMAGE'))
+    if not self.getSetting("VIBE_IMAGE"):
+      self.setSetting("VIBE_IMAGE", config.get('Series Descriptions', 'VIBE_IMAGE'))
+
+    if not self.getSetting("Rating_Enabled"):
+      self.setSetting("Rating_Enabled", config.getboolean('Rating', 'Enabled'))
+    if not self.getSetting("Maximum_Rating_Score"):
+      self.setSetting("Maximum_Rating_Score", config.getint('Rating', 'Maximum_Score'))
+
+    if not self.getSetting("Color_File_Name") or not os.path.exists(self.getSetting("Color_File_Name")):
+      colorFilename = config.get('Color File', 'Filename')
+      self.setSetting("Color_File_Name", os.path.join(os.path.dirname(inspect.getfile(self.__class__)),
                                                       '../Resources/Colors', colorFilename))
