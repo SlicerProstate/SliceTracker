c5f56ba173109a48b951553350ed6cb28bfdab42
Christian Herz <christianherz1985@googlemail.com>
2017-12-09 02:26:43 -0500

    ENH: Improved segmentation validation dialog (issue #321)
    * not using red slice view anymore
    * initial size is 2/3 of width and height of the main window
    * basic code refactoring

--- SliceTracker/SliceTrackerUtils/steps/plugins/segmentationValidator.py
+++ SliceTracker/SliceTrackerUtils/steps/plugins/segmentationValidator.py
@@ -1,94 +1,109 @@
 import ctk
 import qt
 import slicer
-
-
+import logging
 
 from SlicerDevelopmentToolboxUtils.events import SlicerDevelopmentToolboxEvents
 from SlicerDevelopmentToolboxUtils.mixins import  ModuleWidgetMixin, ModuleLogicMixin
 from SlicerDevelopmentToolboxUtils.icons import Icons
+from SlicerDevelopmentToolboxUtils.decorators import logmethod
 
 
-
-class SliceTrackerSegmentationValidatorPlugin(ModuleWidgetMixin):
+class SliceTrackerSegmentationValidatorPlugin(qt.QDialog, ModuleWidgetMixin):
 
   FinishedEvent = SlicerDevelopmentToolboxEvents.FinishedEvent
   CanceledEvent = SlicerDevelopmentToolboxEvents.CanceledEvent
 
   def __init__(self, inputVolume, labelNode):
-    super(SliceTrackerSegmentationValidatorPlugin, self).__init__()
-    self.inputVolume = inputVolume
+    qt.QDialog.__init__(self)
+    self.className = self.__class__.__name__
+    self.setWindowTitle("Modify Segmentation")
+    self.setWindowFlags(qt.Qt.WindowStaysOnTopHint)
+    self.volumeNode = inputVolume
     self.labelNode = labelNode
-    self.segmentationModified = False
-    self.observedSegmentation = None
+    self._initializeMembers()
     self.setup()
 
+  def _initializeMembers(self):
+    self.segmentationModified = False
+    self.observedSegmentation = None
+
   def setup(self):
-    self.validationWindow = qt.QDialog()
-    self.validationWindow.setWindowTitle("Modify Segmentation")
-    self.validationWindow.setWindowFlags(qt.Qt.WindowStaysOnTopHint)
-    self.validationWindow.setLayout(qt.QGridLayout())
-
-    self.segmentEditorWidget = slicer.qMRMLSegmentEditorWidget()
-    self.segmentationNode = slicer.vtkMRMLSegmentationNode()
-    slicer.mrmlScene.AddNode(self.segmentationNode)
-    self.segmentationNode.CreateDefaultDisplayNodes()  # only needed for display
-    self.segmentationNode.SetReferenceImageGeometryParameterFromVolumeNode(self.inputVolume)
-
-    self.segmentEditorWidget.setMRMLScene(slicer.mrmlScene)
-    self.segmentEditorNode = slicer.vtkMRMLSegmentEditorNode()
-    slicer.mrmlScene.AddNode(self.segmentEditorNode)
-    self.segmentEditorWidget.setMRMLSegmentEditorNode(self.segmentEditorNode)
-    self.segmentEditorWidget.hide()
-
-    self.segmentEditorWidget.setSegmentationNodeSelectorVisible(False)
-    self.segmentEditorWidget.setMasterVolumeNodeSelectorVisible(False)
-    self.segmentEditorWidget.setSwitchToSegmentationsButtonVisible(False)
-    self.segmentEditorWidget.findChild(slicer.qMRMLSegmentsTableView, "SegmentsTableView").hide()
-    self.segmentEditorWidget.findChild(qt.QPushButton, "AddSegmentButton").hide()
-    self.segmentEditorWidget.findChild(qt.QPushButton, "RemoveSegmentButton").hide()
-    self.segmentEditorWidget.findChild(ctk.ctkMenuButton, "Show3DButton").hide()
-
-    widget = slicer.app.layoutManager().sliceWidget("Red")
-    #self.segmentEditorWidget.setFixedWidth(self.segmentEditorWidget.minimumSizeHint.width())
-    if not widget:
-      raise AttributeError("Slice widget with name %s not found" % self.widgetName)
-    sliceNode = widget.sliceLogic().GetSliceNode()
-    sliceNode.SetOrientationToAxial()
-    sliceNode.RotateToVolumePlane(self.inputVolume)
-
-    sliceLogic = widget.sliceLogic()
-    sliceLogic.FitSliceToAll()
-    FOV = sliceLogic.GetSliceNode().GetFieldOfView()
-    self.setFOV(sliceLogic, FOV)
-
-    self.sliceWidget = slicer.qMRMLSliceWidget()
-    #self.sliceWidget.resize(1025, 723)
-    self.sliceWidget.setMRMLScene(widget.mrmlScene())
-    self.sliceWidget.setMRMLSliceNode(sliceNode)
-    self.sliceWidget.resize(1025, 723)
+    self.setLayout(qt.QGridLayout())
+    self.setupSliceWidget()
 
     iconSize = qt.QSize(36, 36)
-
-    self.modifySegmentButton = self.createButton("Modify Segmentation", icon=slicer.modules.segmenteditor.icon, iconSize=iconSize)
+    self.modifySegmentButton = self.createButton("Modify Segmentation", icon=slicer.modules.segmenteditor.icon,
+                                                 iconSize=iconSize)
     self.confirmSegmentButton = self.createButton("Confirm Segmentation", icon=Icons.apply, iconSize=iconSize)
     self.cancelButton = self.createButton("Cancel", icon=Icons.cancel, iconSize=iconSize)
 
-    buttonLayout = self.createHLayout([self.modifySegmentButton,self.confirmSegmentButton,self.cancelButton])
+    self.setupSegmentEditor()
+    self.layout().addWidget(self.segmentEditorWidget, 0, 0, 2, 1)
+    self.layout().addWidget(self.sliceWidget, 0, 1)
+    self.layout().addWidget(self.createHLayout([self.modifySegmentButton,self.confirmSegmentButton,self.cancelButton]),
+                            1, 1)
+    self.setupConnections()
 
-    self.validationWindow.layout().addWidget(self.segmentEditorWidget, 0, 0)
-    self.validationWindow.layout().addWidget(self.sliceWidget, 0, 1)
-    self.validationWindow.layout().addWidget(buttonLayout, 1, 1)
+  def setupConnections(self):
+    self.modifySegmentButton.clicked.connect(self.onModifySegmentButtonClicked)
+    self.confirmSegmentButton.clicked.connect(self.onConfirmSegmentButtonClicked)
+    self.cancelButton.clicked.connect(lambda pushed: self.close())
 
-    self.modifySegmentButton.connect('clicked()', self.onModifySegmentButtonClicked)
-    self.confirmSegmentButton.connect('clicked()', self.onConfirmSegmentButtonClicked)
-    self.cancelButton.connect('clicked()', self.onCancelButtonClicked)
+  def setupSliceWidget(self):
+    self.sliceNode = slicer.mrmlScene.CreateNodeByClass('vtkMRMLSliceNode')
+    self.sliceNode.SetName("Black")
+    self.sliceNode.SetLayoutName("Black")
+    self.sliceNode.SetLayoutLabel("BL")
+    self.sliceNode.SetOrientationToAxial()
+    slicer.mrmlScene.AddNode(self.sliceNode)
+    self.sliceWidget = self.layoutManager.viewWidget(self.sliceNode)
 
+    self.sliceLogic = slicer.app.applicationLogic().GetSliceLogic(self.sliceNode)
+    self.sliceNode.SetMappedInLayout(1)
+
+  def setupSegmentEditor(self):
+    self.segmentEditorWidget = slicer.qMRMLSegmentEditorWidget()
+    self.segmentEditorWidget.setMRMLScene(slicer.mrmlScene)
+    self.segmentEditorWidget.visible = False
+    self.segmentEditorWidget.setSegmentationNodeSelectorVisible(False)
+    self.segmentEditorWidget.setMasterVolumeNodeSelectorVisible(False)
+    self.segmentEditorWidget.setSwitchToSegmentationsButtonVisible(False)
+    self.segmentEditorWidget.findChild(qt.QPushButton, "AddSegmentButton").hide()
+    self.segmentEditorWidget.findChild(qt.QPushButton, "RemoveSegmentButton").hide()
+    self.segmentEditorWidget.findChild(ctk.ctkMenuButton, "Show3DButton").hide()
+    self.segmentEditorWidget.findChild(ctk.ctkExpandableWidget, "SegmentsTableResizableFrame").hide()
+    self.segmentEditorWidget.setSizePolicy(qt.QSizePolicy.Maximum, qt.QSizePolicy.Expanding)
+
+  def _initializeSegmentationNode(self):
+    self.segmentationNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLSegmentationNode")
+    self.segmentationNode.CreateDefaultDisplayNodes()
+    self.segmentationNode.SetReferenceImageGeometryParameterFromVolumeNode(self.volumeNode)
+
+  def _initializeSegmentEditorNode(self):
+    self.segmentEditorNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLSegmentEditorNode")
+    self.segmentEditorWidget.setMRMLSegmentEditorNode(self.segmentEditorNode)
+
+  @logmethod(logging.DEBUG)
   def run(self):
     self.segmentationModified = False
-    prompt = self.validationWindow.exec_()
-    if prompt == qt.QDialog.Rejected:
+    self._initializeSegmentationNode()
+    self._initializeSegmentEditorNode()
+
+    self.sliceNode.SetMappedInLayout(1)
+    self.sliceLogic.GetSliceCompositeNode().SetBackgroundVolumeID(self.volumeNode.GetID())
+    self.sliceLogic.GetSliceCompositeNode().SetLabelVolumeID(self.labelNode.GetID())
+    self.sliceLogic.FitSliceToAll()
+    self.sliceNode.RotateToVolumePlane(self.volumeNode)
+    self.resize(int(slicer.util.mainWindow().width/3*2), int(slicer.util.mainWindow().height/3*2))
+    result = self.exec_()
+    if result == qt.QDialog.Rejected:
+      logging.debug("{}: Dialog got rejected.".format(self.className))
       self.invokeEvent(self.CanceledEvent)
+    elif result == qt.QDialog.Accepted:
+      logging.debug("{}: Dialog got approved.".format(self.className))
+      self.invokeEvent(self.FinishedEvent, self.labelNode)
+    self.cleanup()
 
   def onModifySegmentButtonClicked(self):
     slicer.modules.segmentations.logic().ImportLabelmapToSegmentationNode(self.labelNode, self.segmentationNode)
@@ -96,25 +111,20 @@ class SliceTrackerSegmentationValidatorPlugin(ModuleWidgetMixin):
     segmentationDisplayNode = self.segmentationNode.GetDisplayNode()
     segmentationDisplayNode.SetSegmentVisibility2DFill(segmentID, False)
     self.segmentEditorWidget.setSegmentationNode(self.segmentationNode)
-    self.segmentEditorWidget.setMasterVolumeNode(self.inputVolume)
+    self.segmentEditorWidget.setMasterVolumeNode(self.volumeNode)
     self.addSegmentationObserver(self.segmentationNode)
     self.segmentEditorWidget.show()
     self.modifySegmentButton.hide()
 
+
   def onConfirmSegmentButtonClicked(self):
     if self.segmentationModified is True:
       slicer.modules.segmentations.logic().ExportAllSegmentsToLabelmapNode(self.segmentationNode, self.labelNode)
-      ModuleLogicMixin.runBRAINSResample(inputVolume=self.labelNode, referenceVolume=self.inputVolume,
+      ModuleLogicMixin.runBRAINSResample(inputVolume=self.labelNode, referenceVolume=self.volumeNode,
                            outputVolume=self.labelNode)
-    self.cleanup()
-    self.validationWindow.accept()
-    self.invokeEvent(self.FinishedEvent, self.labelNode)
+    self.accept()
 
-  def onCancelButtonClicked(self):
-    self.validationWindow.close()
-    self.cleanup()
-    #self.invokeEvent(self.CanceledEvent)
-
+  @logmethod(logging.DEBUG)
   def addSegmentationObserver(self, segmentation):
     import vtkSegmentationCorePython as vtkSegmentationCore
     self.observedSegmentation = segmentation
@@ -122,6 +132,7 @@ class SliceTrackerSegmentationValidatorPlugin(ModuleWidgetMixin):
       vtkSegmentationCore.vtkSegmentation.RepresentationModified,
       self.onSegmentModified)
 
+  @logmethod(logging.DEBUG)
   def removeSegmentationObserver(self):
     if self.observedSegmentation:
       self.observedSegmentation.RemoveObserver(self.segmentObserver)
@@ -130,8 +141,12 @@ class SliceTrackerSegmentationValidatorPlugin(ModuleWidgetMixin):
   def onSegmentModified(self, caller, event):
     self.segmentationModified = True
 
+  @logmethod(logging.DEBUG)
   def cleanup(self):
     self.removeSegmentationObserver()
     if self.observedSegmentation:
       slicer.mrmlScene.RemoveNode(self.segmentationNode)
       slicer.mrmlScene.RemoveNode(self.segmentEditorNode)
+    self.observedSegmentation = None
+    self.segmentEditorWidget.hide()
+    self.modifySegmentButton.show()
